8ba10693980992e5f0ac9236f120c41fba283269
Lakshan Bandara <lakshanwarunab@gmail.com>
2020-09-29 17:17:32 +0530

    add-get-auth-url

--- fileUploadWebAppBackend/index.js
+++ fileUploadWebAppBackend/index.js
@@ -10,7 +10,7 @@ const credentialsFile = require('./credentialsFile.json');
 const client_id = credentialsFile.web.client_id;
 const client_secret = credentialsFile.web.client_secret;
 const redirect_uris = credentialsFile.web.redirect_uris;
-const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+const client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
 
 const SCOPE = ['https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/drive.file']
 
@@ -19,6 +19,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Sri file upload backend runnning'));
+
+app.get('/getauthorizationurl', (request, response) => {
+    const authorizationUrl = client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPE,
+    });
+    console.log(authorizationUrl);
+    return response.send(authorizationUrl);
+});
 
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => console.log(`Server has Started ${PORT}`));
